e32fc7cafeafc3f78f0833d51bd6eda81de7c9ef
Bogomolov Ivan <ibogomolov@arello-mobile.com>
2016-05-30 16:38:16 +0600

    [bugfix][WDT-138] clean unused files

--- swg2rst/swagger/securityMixin.py
+++
@@ -1,8 +0,0 @@
-class SecurityMixin(object):
-
-    security = None
-
-    def _fill_securities(self, obj):
-        self.security = {}
-        for security in obj:
-            self.security.update(security)
